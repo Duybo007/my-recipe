c6dcd308c7b50e0b33e5936a483cc7b795eddfe9
duybo <duybo95@gmail.com>
2022-11-11 16:12:44 -0800

    key

--- pages/Search/[searched].js
+++ pages/Search/[searched].js
@@ -30,7 +30,7 @@ function Searched() {
 
   <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
     {search.map((recipe) => (
-      <Link href={"/Recipe/"+recipe.id}><a key={recipe.id}  className="group">
+      <Link key={recipe.id} href={"/Recipe/"+recipe.id}><a key={recipe.id}  className="group">
         
         <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-w-7 xl:aspect-h-8">
           <img
